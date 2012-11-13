62692413818394664148831eeab2ce3067b99799
Inna Krasnitskaya <innakiv89@mail.ru>
2012-11-14 00:20:36 +0300

    Redirect isn't working

--- src/main/java/org/travelplan/controller/SecurityController.java
+++ src/main/java/org/travelplan/controller/SecurityController.java
@@ -1,11 +1,11 @@
 package org.travelplan.controller;
 
-import org.springframework.stereotype.Controller;
+/*import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
+*/
 //@Controller
 public class SecurityController {
 	
